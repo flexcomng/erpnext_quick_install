a9a8f04b0d076a84944d0e8712517b79ee897879
Felix Akhigbe <feel_me2ky@yahoo.com>
2023-11-03 19:41:36 +0000

    pre-install refactor

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Define colors if needed for the echo statements
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 ask_twice() {
     local prompt="$1"
@@ -40,18 +39,14 @@ ask_twice() {
         fi
     done
 }
-# Function to check and install Ansible
 install_ansible() {
     if ! command -v ansible-playbook >/dev/null; then
         echo "Ansible is not installed. Installing Ansible..."
 
-        # Update package lists
         sudo apt update
 
-        # Install software-properties-common if not already installed
         sudo apt install -y software-properties-common
 
-        # Add Ansible PPA (Personal Package Archive) and install Ansible
         sudo apt-add-repository --yes --update ppa:ansible/ansible
         sudo apt install -y ansible
 
@@ -60,23 +55,18 @@ install_ansible() {
         echo "Ansible is already installed."
     fi
 }
-# Collect SQL root password and set environment variable
 echo -e "${YELLOW}Now let's set some important parameters...${NC}"
 sleep 1
 echo -e "${YELLOW}We will need your required SQL root password${NC}"
 sleep 1
 sqlpasswrd=$(ask_twice "What is your required SQL root password" "true")
-export MARIADB_ROOT_PASSWORD="$sqlpasswrd" # Use the collected password here
-
-# Function to run the Ansible playbook
+export MARIADB_ROOT_PASSWORD="$sqlpasswrd"
+echo -e "\n"
 run_playbook() {
-    # Execute the playbook
     echo "Running Ansible playbook..."
     ansible-playbook playbooks/main.yml -vvv
 }
-
-# Check if Ansible is installed and install if not
+echo -e "\n"
 install_ansible
 
-# Run the playbook
 run_playbook
