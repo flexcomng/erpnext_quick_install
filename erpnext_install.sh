2b96eaec973199db454e00219d4160037e90c8cb
Felix Akhigbe <feel_me2ky@yahoo.com>
2024-02-29 01:49:21 +0000

    reversal: handled ssl management with certbot

--- erpnext_install.sh
+++ erpnext_install.sh
@@ -435,10 +435,7 @@ case "$continue_prod" in
             # Obtain and Install the certificate
             echo -e "${YELLOW}Obtaining and installing SSL certificate...${NC}"
             sleep 2
-            bench config dns_multitenant on && \
-            yes | bench setup nginx && \
-            sudo service nginx reload && \
-            sudo bench setup lets-encrypt $site_name --non-interactive --agree-tos --email $email_address 
+            sudo certbot --nginx --non-interactive --agree-tos --email $email_address -d $site_name
             echo -e "${GREEN}SSL certificate installed successfully.${NC}"
             sleep 2
             ;;
